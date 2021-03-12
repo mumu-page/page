850bad4809121fa289582b466c31a4fa0d18a4b3
向仕林 <295755973@qq.com>
2021-03-12 21:01:15 +0800

    🎈perf: 优化编辑器样式

--- src/components/CodeEditor/index.tsx
+++ src/components/CodeEditor/index.tsx
@@ -7,12 +7,12 @@ import React, {
   useCallback,
   useMemo,
   memo,
-} from "react";
-import * as monaco from "monaco-editor";
-import { CodeEditorInstanceProps, CodeEditorProps } from "./typings";
-import { Spin } from "antd";
-import { isEqual } from "lodash";
-import "./index.scss";
+} from 'react'
+import * as monaco from 'monaco-editor'
+import { CodeEditorInstanceProps, CodeEditorProps } from './typings'
+import { Spin } from 'antd'
+import { isEqual } from 'lodash'
+import './index.scss'
 
 // 禁用了语法错误，但无法获得语法高亮支持 tsx
 monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
@@ -23,18 +23,18 @@ monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
   noEmit: true,
   esModuleInterop: true,
   jsx: monaco.languages.typescript.JsxEmit.React,
-  reactNamespace: "React",
+  reactNamespace: 'React',
   allowJs: true,
-  typeRoots: ["node_modules/@types"],
-});
+  typeRoots: ['node_modules/@types'],
+})
 monaco.languages.typescript.typescriptDefaults.setDiagnosticsOptions({
   noSemanticValidation: false,
   noSyntaxValidation: false,
-});
+})
 const defaultOptions = {
-  theme: "vs-dark",
-  language: "typescript",
-};
+  theme: 'vs-dark',
+  language: 'typescript',
+}
 const CodeEditor = forwardRef(
   (
     props: CodeEditorProps,
@@ -44,105 +44,95 @@ const CodeEditor = forwardRef(
       | null
   ) => {
     const {
-      code = "",
+      code = '',
       options: optionProp = defaultOptions,
       onChangeCode,
-    } = props;
-    const [spinning, setSpinning] = useState(true);
-    const element = useRef<HTMLDivElement>(null);
-    const editor = useRef<monaco.editor.IStandaloneCodeEditor>();
+    } = props
+    const [spinning, setSpinning] = useState(true)
+    const element = useRef<HTMLDivElement>(null)
+    const editor = useRef<monaco.editor.IStandaloneCodeEditor>()
     const options = useMemo(() => ({ ...defaultOptions, ...optionProp }), [
       optionProp,
-    ]);
+    ])
 
     const onLayoutChange = useCallback(() => {
-      const { offsetHeight, offsetWidth } = element.current || {};
+      const { offsetHeight, offsetWidth } = element.current || {}
       if (offsetHeight && offsetWidth) {
         editor.current?.layout({
           width: offsetWidth,
           height: offsetHeight,
-        });
+        })
       }
-    }, []);
+    }, [])
 
     const formatDocument = () => {
-      (editor?.current?.getAction(
-        "editor.action.formatDocument"
-      ) as any)?._run();
-    };
+      editor.current?.getAction?.('editor.action.formatDocument')?.run()
+    }
 
     const create = useCallback(
-      (el: HTMLElement, value: string, options?: object) => {
+      (el: HTMLElement, value: string, options = {}) => {
         const _options = {
           ...defaultOptions,
-          ...(options || {}),
-        };
+          ...options,
+        }
         editor.current = monaco.editor.create(el, {
           value,
           ..._options,
-        });
+        })
+        editor.current.getAction('editor.action.formatDocument').run()
         setTimeout(() => {
-          formatDocument();
-        }, 500)
+          formatDocument()
+        }, 300)
         editor.current.onDidChangeModelContent(() => {
-          onChangeCode && onChangeCode(editor.current?.getValue());
-        });
+          onChangeCode?.(editor.current?.getValue?.())
+        })
       },
       [onChangeCode]
-    );
+    )
 
     useImperativeHandle(
       ref,
       () => ({
         editor: editor.current,
         mount(el: HTMLDivElement, code: string) {
-          create(el, code);
+          create(el, code)
         },
         resetMount(code: string) {
           if (element.current) {
-            editor.current?.dispose();
-            create(element.current, code);
+            editor.current?.dispose()
+            create(element.current, code)
           }
         },
         setCode(code: string) {
-          editor.current?.setValue(code);
+          editor.current?.setValue(code)
         },
       }),
       [create]
-    );
+    )
 
     useEffect(() => {
-      setSpinning(true);
-      setTimeout(() => {
-        if (element.current) {
-          editor.current?.dispose();
-          create(element.current, code, options);
-          setSpinning(false);
-        }
-      });
-    }, []);
-
-    useEffect(() => {
-      window?.addEventListener("resize", onLayoutChange);
+      setSpinning(true)
+      if (element.current) {
+        create(element.current, code, options)
+        setSpinning(false)
+      }
+      window?.addEventListener('resize', onLayoutChange)
       return () => {
-        window?.removeEventListener("resize", onLayoutChange);
-        editor.current?.dispose();
-      };
-    }, [onLayoutChange]);
+        editor.current?.dispose?.()
+        window?.removeEventListener('resize', onLayoutChange)
+      }
+    }, [])
 
     return (
       <div className="container">
         {/* 当编辑器插件还没没有加载完成，是没有dom样式的，所以不能直接包裹它 */}
-        <Spin
-          spinning={spinning}
-          className="code-spinning"
-        />
+        <Spin spinning={spinning} className="code-spinning" />
         <div className="code-editor" ref={element}></div>
       </div>
-    );
+    )
   }
-);
+)
 
 export default memo(CodeEditor, (prevProps: any, nextProps: any) => {
-  return isEqual(prevProps, nextProps);
-});
+  return isEqual(prevProps, nextProps)
+})
