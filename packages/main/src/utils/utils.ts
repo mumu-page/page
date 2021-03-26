44d533f3b5778dbb18f99b673413765d0beed7d5
feline <295755973@qq.com>
2021-03-26 11:46:03 +0800

    🐞fix: standalone好像不能作为生产依赖

--- packages/main/src/utils/utils.ts
+++ packages/main/src/utils/utils.ts
@@ -1,5 +1,4 @@
-import React from 'react'
-import { transform } from '@babel/standalone'
+import React from "react";
 import {
   Input,
   InputNumber,
@@ -17,13 +16,13 @@ import {
   Col,
   Button,
   Form,
-} from 'antd'
-import { ICONS } from '../constants'
-import { IFormComProp } from '../stores/typings'
-import { Target_ClassName } from '../constants/constants'
-import { SET_MOVEABLE_OPTIONS } from '../stores/action-type'
+} from "antd";
+import { ICONS } from "../constants";
+import { IFormComProp } from "../stores/typings";
+import { Target_ClassName } from "../constants/constants";
+import { SET_MOVEABLE_OPTIONS } from "../stores/action-type";
 
-const { TimePicker: TP, ...OtherDatePickerCom } = DatePicker
+const { TimePicker: TP, ...OtherDatePickerCom } = DatePicker;
 const context = {
   React,
   Form,
@@ -47,85 +46,86 @@ const context = {
   Col,
   Button,
   ...ICONS,
-}
+};
 
 export function guid() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
     var r = (Math.random() * 16) | 0,
-      v = c === 'x' ? r : (r & 0x3) | 0x8
-    return v.toString(16)
-  })
+      v = c === "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
 }
 
 export function isSelect(componentKey: string) {
   return [
-    'Select',
-    'Cascader',
-    'TimePicker',
-    'TimePicker.RangePicker',
-    'DatePicker',
-    'DatePicker.RangePicker',
-  ].includes(componentKey)
+    "Select",
+    "Cascader",
+    "TimePicker",
+    "TimePicker.RangePicker",
+    "DatePicker",
+    "DatePicker.RangePicker",
+  ].includes(componentKey);
 }
 
 export function hasNotPlaceholder(componentKey: string) {
   return [
-    'Radio',
-    'Checkbox',
-    'Switch',
-    'Slider',
-    'Rate',
-    'Upload',
-    'Row',
-    'Button',
-  ].includes(componentKey)
+    "Radio",
+    "Checkbox",
+    "Switch",
+    "Slider",
+    "Rate",
+    "Upload",
+    "Row",
+    "Button",
+  ].includes(componentKey);
 }
 
 export function isDatePicker(componentKey: string) {
   return [
-    'TimePicker',
-    'TimePicker.RangePicker',
-    'DatePicker',
-    'DatePicker.RangePicker',
-  ].includes(componentKey)
+    "TimePicker",
+    "TimePicker.RangePicker",
+    "DatePicker",
+    "DatePicker.RangePicker",
+  ].includes(componentKey);
 }
 
 export function isRenderFormItem(componentKey: string) {
-  return !['Col', 'Button'].includes(componentKey)
+  return !["Col", "Button"].includes(componentKey);
 }
 
 export function isColComponent(componentKey: string) {
-  return ['Col'].includes(componentKey)
+  return ["Col"].includes(componentKey);
 }
 
 export function isDatePickerRange(componentKey: string) {
-  return ['TimePicker.RangePicker', 'DatePicker.RangePicker'].includes(
+  return ["TimePicker.RangePicker", "DatePicker.RangePicker"].includes(
     componentKey
-  )
+  );
 }
 
 export async function string2Component(input?: string) {
-  if (!input) return ''
+  if (!input) return "";
   try {
-    let output = transform(`<>${input}</>`, {
-      presets: ['react', 'es2015'],
-    }).code
-    output = output?.replace('"use strict";', '').trim()
+    type IW = Window & typeof globalThis & { Babel: any };
+    let output = (window as IW).Babel.transform(`<>${input}</>`, {
+      presets: ["react", "es2015"],
+    }).code;
+    output = output?.replace('"use strict";', "").trim();
     // eslint-disable-next-line no-new-func
     const func = new Function(
-      'context',
+      "context",
       `with(context){const ret = ${output};return ret;}`
-    )
-    return func(context)
+    );
+    return func(context);
   } catch (e) {
-    console.log('e', e)
+    console.log("e", e);
     // eslint-disable-next-line no-throw-literal
-    throw '代码运行错误'
+    throw "代码运行错误";
   }
 }
 
 export function isCheck(componentKey: string) {
-  return ['Radio', 'Checkbox', 'Switch'].includes(componentKey)
+  return ["Radio", "Checkbox", "Switch"].includes(componentKey);
 }
 
 /**
@@ -140,16 +140,16 @@ export function decodeKey(
   result = {} as any
 ) {
   keys.forEach((key) => {
-    result[key] = {}
-  })
+    result[key] = {};
+  });
   try {
     Object.keys(target).forEach((key) => {
-      const [parentKey, childKey] = key?.split('.')
-      const val = target[key]
-      result[parentKey][childKey] = val
-    })
+      const [parentKey, childKey] = key?.split(".");
+      const val = target[key];
+      result[parentKey][childKey] = val;
+    });
   } finally {
-    return result
+    return result;
   }
 }
 
@@ -161,24 +161,24 @@ export function decodeKey(
  */
 export function encodeKey(
   target1: {
-    key: string
-    data: { [key: string]: any }
+    key: string;
+    data: { [key: string]: any };
   },
   target2: {
-    key: string
-    data: { [key: string]: any }
+    key: string;
+    data: { [key: string]: any };
   },
   result = {} as any
 ) {
   try {
     Object.keys(target1.data).forEach((key) => {
-      result[`${target1.key}.${key}`] = target1.data[key]
-    })
+      result[`${target1.key}.${key}`] = target1.data[key];
+    });
     Object.keys(target2.data).forEach((key) => {
-      result[`${target2.key}.${key}`] = target2.data[key]
-    })
+      result[`${target2.key}.${key}`] = target2.data[key];
+    });
   } finally {
-    return result
+    return result;
   }
 }
 
@@ -195,32 +195,32 @@ export function findTarget(
 ) {
   let elementGuidelines = [].slice.call(
     document.querySelectorAll(selectors) as any
-  )
+  );
   const target = elementGuidelines.filter((item: any) => {
-    return item?.getAttribute?.('data-id') === id
-  })?.[0]
+    return item?.getAttribute?.("data-id") === id;
+  })?.[0];
   const frame = list?.filter((item) => {
-    return item.id === id
-  })?.[0]?.layout?.frame
+    return item.id === id;
+  })?.[0]?.layout?.frame;
 
   // 将当前元素从divList中移除, 当前元素不再作为参考辅助线
   elementGuidelines = elementGuidelines.filter((item: any) => {
-    return item?.getAttribute?.('data-id') !== id
-  })
+    return item?.getAttribute?.("data-id") !== id;
+  });
 
   return {
     elementGuidelines,
     target,
     frame,
-  }
+  };
 }
 
 // 重新获取当前选中元素
 export function refreshTarget(
   target: unknown,
   commonDispatch: React.Dispatch<{
-    type: string
-    payload?: object | undefined
+    type: string;
+    payload?: object | undefined;
   }>
 ) {
   requestAnimationFrame(() => {
@@ -229,12 +229,12 @@ export function refreshTarget(
       payload: {
         target: null, // immner会认为targer是同一个元素，导致不更新
       },
-    })
+    });
     commonDispatch({
       type: SET_MOVEABLE_OPTIONS,
       payload: {
         target,
       },
-    })
-  })
+    });
+  });
 }
