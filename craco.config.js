957b53f84e6eb9bafe65783aacb345623435fc2f
feline <295755973@qq.com>
2021-04-06 18:39:48 +0800

    🐳chore: adjust craco.config

--- craco.config.js
+++ craco.config.js
@@ -1,8 +1,36 @@
-const CracoLessPlugin = require('craco-less')
-const MonacoEditorWebpackPlugin = require('monaco-editor-webpack-plugin')
+const CracoLessPlugin = require("craco-less");
+const MonacoEditorWebpackPlugin = require("monaco-editor-webpack-plugin");
+const path = require('path');
+const APP_DIR = path.resolve(__dirname, './src');
+const MONACO_DIR = path.resolve(__dirname, './node_modules/monaco-editor');
 
 module.exports = {
   webpack: {
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          include: APP_DIR,
+          use: [
+            {
+              loader: "style-loader",
+            },
+            {
+              loader: "css-loader",
+              options: {
+                modules: true,
+                namedExport: true,
+              },
+            },
+          ],
+        },
+        {
+          test: /\.css$/,
+          include: MONACO_DIR,
+          use: ["style-loader", "css-loader"],
+        },
+      ],
+    },
     plugins: [new MonacoEditorWebpackPlugin()],
   },
   plugins: [
@@ -18,4 +46,4 @@ module.exports = {
       },
     },
   ],
-}
+};
