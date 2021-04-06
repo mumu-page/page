ae41b32a7760588984e51836356ded3d51d08913
feline <295755973@qq.com>
2021-04-06 21:42:35 +0800

    🐞fix: 排除一些不需要的参数

--- src/layouts/EditorArea/ComponentList/index.tsx
+++ src/layouts/EditorArea/ComponentList/index.tsx
@@ -1,92 +1,97 @@
-import React, { useCallback, useRef } from 'react'
-import { Col, Form, Row } from 'antd'
-import { useEffect } from 'react'
-import ComponentItem from '../ComponentItem'
-import { ContextMenu } from '../../../components'
-import shortid from 'shortid'
+import React, { useCallback, useRef } from "react";
+import { Col, Form, Row } from "antd";
+import { useEffect } from "react";
+import ComponentItem from "../ComponentItem";
+import { ContextMenu } from "../../../components";
+import shortid from "shortid";
 import {
   CopyOutlined,
   SettingOutlined,
   DeleteOutlined,
   VerticalAlignTopOutlined,
   VerticalAlignBottomOutlined,
-} from '@ant-design/icons'
-import { ICommonDispatch, IFormComProp } from '../../../stores/typings'
-import { findTarget, isDatePicker } from '../../../utils/utils'
-import { Target_ClassName } from '../../../constants'
+} from "@ant-design/icons";
+import { ICommonDispatch, IFormComProp } from "../../../stores/typings";
+import { findTarget, isDatePicker } from "../../../utils/utils";
+import { Target_ClassName } from "../../../constants";
 import {
   COPY_COMPONENT_LIST,
   DELETE_TARGET,
   DEL_COMPONENT_LIST,
   SET_MOVEABLE_OPTIONS,
   SET_TARGET_BY_COMPONENT_LIST,
-} from '../../../stores/action-type'
-import Menu from '../../../components/ContextMenu/Menu'
+} from "../../../stores/action-type";
+import Menu from "../../../components/ContextMenu/Menu";
 import {
   INFINITEVIEWER_SCROLL,
   SHOW_SETTING_PANL,
-} from '../../../constants/events'
-import eventBus from '../../../utils/eventBus'
-import './index.less'
+} from "../../../constants/events";
+import eventBus from "../../../utils/eventBus";
+import "./index.less";
 
 interface EditorAreaProps extends ICommonDispatch<object> {
-  componentList: IFormComProp[]
-  target: IFormComProp
+  componentList: IFormComProp[];
+  target: IFormComProp;
 }
 
 enum HANDLE_TYPE {
-  copy = '复制这个',
-  setting = '设置属性',
-  del = '删除这个',
-  toTop = '上移',
-  topBottom = '下移',
+  copy = "复制这个",
+  setting = "设置属性",
+  del = "删除这个",
+  toLeft = "左移",
+  toRight = "右移",
 }
 
 const options = [
   {
-    key: 'copy',
+    key: "copy",
     icon: <CopyOutlined />,
     label: HANDLE_TYPE.copy,
   },
   {
-    key: 'setting',
+    key: "setting",
     icon: <SettingOutlined />,
     label: HANDLE_TYPE.setting,
   },
   {
-    key: 'del',
+    key: "del",
     icon: <DeleteOutlined />,
     label: HANDLE_TYPE.del,
-    type: 'del',
+    type: "del",
   },
   {
-    key: 'toTop',
+    key: "toTop",
     icon: <VerticalAlignTopOutlined />,
-    label: HANDLE_TYPE.toTop,
-    type: 'toTop',
+    label: HANDLE_TYPE.toLeft,
+    type: "toTop",
   },
   {
-    key: 'topBottom',
+    key: "topBottom",
     icon: <VerticalAlignBottomOutlined />,
-    label: HANDLE_TYPE.topBottom,
-    type: 'topBottom',
+    label: HANDLE_TYPE.toRight,
+    type: "topBottom",
   },
-]
+];
 
 export default function Container(props: EditorAreaProps) {
-  const { target, componentList = [], commonDispatch } = props
-  const [form] = Form.useForm()
-  const contenxtMenu = useRef(null)
+  const { target, componentList = [], commonDispatch } = props;
+  const [form] = Form.useForm();
+  const contenxtMenu = useRef(null);
   const {
     componentProps,
     formItemProps,
     formProps = {},
     colProps = {},
     rowProps = {},
-  } = target
+  } = target;
+
+  const {
+    colNum,
+    ...otherRow
+  } = rowProps;
 
   const setMoveableOption = (id: string | number) => {
-    const { elementGuidelines, target, frame } = findTarget(id, componentList)
+    const { elementGuidelines, target, frame } = findTarget(id, componentList);
     commonDispatch({
       type: SET_MOVEABLE_OPTIONS,
       payload: {
@@ -100,8 +105,8 @@ export default function Container(props: EditorAreaProps) {
         draggable: false,
         resizable: false,
       },
-    })
-  }
+    });
+  };
 
   const handleContextMenuClick = (key: string, label: string) => {
     if (label === HANDLE_TYPE.del) {
@@ -110,78 +115,78 @@ export default function Container(props: EditorAreaProps) {
         payload: {
           id: target.id,
         },
-      })
+      });
       commonDispatch({
         type: DELETE_TARGET,
-      })
+      });
       commonDispatch({
         type: SET_MOVEABLE_OPTIONS,
         payload: {
           target: null,
         },
-      })
+      });
     }
     if (label === HANDLE_TYPE.setting) {
-      eventBus.emit(SHOW_SETTING_PANL)
+      eventBus.emit(SHOW_SETTING_PANL);
     }
     if (label === HANDLE_TYPE.copy) {
-      const newId = shortid()
+      const newId = shortid();
       commonDispatch({
         type: COPY_COMPONENT_LIST,
         payload: {
           id: target.id,
           newId,
         },
-      })
+      });
       requestAnimationFrame(() => {
-        setMoveableOption(newId)
-      })
+        setMoveableOption(newId);
+      });
     }
-  }
+  };
 
   const onScroll = useCallback(() => {
-    ;(contenxtMenu.current as any)?.hide?.()
-  }, [])
+    (contenxtMenu.current as any)?.hide?.();
+  }, []);
 
   useEffect(() => {
-    setMoveableOption(target.id)
-    eventBus.addListener(INFINITEVIEWER_SCROLL, onScroll)
+    setMoveableOption(target.id);
+    eventBus.addListener(INFINITEVIEWER_SCROLL, onScroll);
     return () => {
-      eventBus.removeListener(INFINITEVIEWER_SCROLL, onScroll)
-    }
-  }, [])
+      eventBus.removeListener(INFINITEVIEWER_SCROLL, onScroll);
+    };
+  }, []);
 
   useEffect(() => {
     if (formItemProps?.name) {
       form.setFieldsValue({
         [formItemProps.name]: componentProps?.defaultValue,
-      })
+      });
     }
-  }, [target.id])
+  }, [target.id]);
 
   useEffect(() => {
-    const _initialValues = {} as any
+    const _initialValues = {} as any;
     componentList.forEach((item) => {
-      const { componentKey, formItemProps, componentProps } = item
-      const { name } = formItemProps || {}
-      const { defaultValue } = componentProps || {}
+      const { componentKey, formItemProps, componentProps } = item;
+      const { name } = formItemProps || {};
+      const { defaultValue } = componentProps || {};
       if (!isDatePicker(componentKey)) {
-        _initialValues[name] = defaultValue
+        _initialValues[name] = defaultValue;
       }
-    })
-    form.setFieldsValue(_initialValues)
-  }, [componentList, form])
+    });
+    form.setFieldsValue(_initialValues);
+  }, [componentList, form]);
 
   return (
     <Form
       {...formProps}
       style={{
-        height: '100%',
-        position: 'relative',
+        height: "100%",
+        position: "relative",
       }}
       form={form}
     >
-      <Row {...rowProps}>
+      <Row {...otherRow}>
         {componentList.map((item: IFormComProp, index: number) => {
           const {
             id,
@@ -191,54 +196,64 @@ export default function Container(props: EditorAreaProps) {
             componentProps,
             colProps: selfColProps = {},
             layout = {},
-          } = item
+          } = item;
 
-          const { frame = { translate: [0, 0, 0] }, height, width } = layout
-          const { translate } = frame
+          const { frame = { translate: [0, 0, 0] }, height, width } = layout;
+          const { translate } = frame;
           const style = {
-            display: 'inline-block',
+            display: "inline-block",
             transform: `translate(${translate[0]}px, ${translate[1]}px)`,
-          } as any
+          } as any;
 
-          if (typeof width === 'number') {
-            style.width = `${width}px`
-          } else if (typeof width === 'string') {
-            style.width = width
+          if (typeof width === "number") {
+            style.width = `${width}px`;
+          } else if (typeof width === "string") {
+            style.width = width;
           }
           if (height) {
-            style.height = `${height}px`
+            style.height = `${height}px`;
           }
 
+          const {
+            align,
+            gutter,
+            justify,
+            wrap,
+            ...otherRowG
+          } = otherRow;
+
+          const {colNum: colNum2, ...otherCol} = selfColProps
+          
           return (
             <Col
               key={id}
               //   style={style}
               {...{
-                ...rowProps,
-                ...selfColProps,
+                ...otherRowG,
+                ...otherCol,
               }}
               onContextMenu={(e) => {
-                e.preventDefault()
+                e.preventDefault();
                 commonDispatch({
                   type: SET_TARGET_BY_COMPONENT_LIST,
                   payload: { id },
-                })
+                });
                 // 无论dom元素如何变，componentList没有变
-                setMoveableOption(id)
-                ;(contenxtMenu.current as any)?.show?.(e)
+                setMoveableOption(id);
+                (contenxtMenu.current as any)?.show?.(e);
               }}
             >
               <div
                 data-id={id}
                 className={Target_ClassName}
                 onClick={(e) => {
-                  e.stopPropagation()
+                  e.stopPropagation();
                   commonDispatch({
                     type: SET_TARGET_BY_COMPONENT_LIST,
                     payload: { id },
-                  })
+                  });
                   // 无论dom元素如何变，componentList没有变
-                  setMoveableOption(id)
+                  setMoveableOption(id);
                 }}
               >
                 <ComponentItem
@@ -255,12 +270,12 @@ export default function Container(props: EditorAreaProps) {
                 />
               </div>
             </Col>
-          )
+          );
         })}
       </Row>
       <ContextMenu ref={contenxtMenu}>
         <Menu options={options} onClick={handleContextMenuClick} />
       </ContextMenu>
     </Form>
-  )
+  );
 }
