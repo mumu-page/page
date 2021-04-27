28fb2bf9b5663201580428b7806eba97db0ab46d
向仕林 <295755973@qq.com>
2021-04-27 20:22:05 +0800

    🦄refactor: 实现发布订阅

--- src/utils/eventBus.ts
+++ src/utils/eventBus.ts
@@ -1,3 +1,50 @@
-import { EventEmitter } from 'events'
+interface ICallback {
+  eventName: string
+  callbacks: (() => void)[]
+}
+
+class EventEmitter {
+  callbacks: ICallback[]
+  constructor() {
+    this.callbacks = []
+  }
+  addListener(eventName: string, fn: () => void) {
+    if (!this.callbacks.length) {
+      this.callbacks = [
+        {
+          eventName,
+          callbacks: [fn],
+        },
+      ]
+    }
+    let flag = false
+    this.callbacks.forEach((item) => {
+      if (item.eventName === eventName) {
+        flag = true
+        item.callbacks.push(fn)
+      }
+    })
+    if (!flag) {
+      this.callbacks.push({
+        eventName,
+        callbacks: [fn],
+      })
+    }
+  }
+  removeListener(eventName: string, fn: () => void) {
+    this.callbacks.forEach((item) => {
+      if (item.eventName === eventName) {
+        item.callbacks = item.callbacks.filter((item) => item !== fn)
+      }
+    })
+  }
+  emit(eventName: string) {
+    this.callbacks.forEach((item) => {
+      if (item.eventName === eventName) {
+        item.callbacks.forEach((fn) => fn())
+      }
+    })
+  }
+}
 
 export default new EventEmitter()
